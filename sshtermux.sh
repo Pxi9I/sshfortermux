78ac126e2b3880c82dbee6ea2f5964df9c3d1aef
Pxi9I <147625984+Pxi9I@users.noreply.github.com>
2026-08-21 21:56:49 +0200

    Update sshtermux.sh

--- sshtermux.sh
+++ sshtermux.sh
@@ -6,8 +6,11 @@ printf "00000000\n00000000\n" | passwd
 sshd
 MY_IP=$(ip route get 1.1.1.1 2>/dev/null | awk '{for(i=1;i<=NF;i++) if($i=="src") print $(i+1)}')
 USER_NAME=$(whoami)
+echo ----------------------------------------------------
 echo "User: ${USER_NAME}"
 echo "IP address: ${MY_IP:-nie znaleziono}"
 echo "port: 8022"
+echo ----------------------------------------------------
 echo "Command to connect:"
 echo "ssh ${USER_NAME}@${MY_IP} -p 8022"
+echo ----------------------------------------------------
